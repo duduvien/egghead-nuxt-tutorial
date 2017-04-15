e3c2025062958209af8c65d1733257855cc3aaf3
duduvien <vivien@vkdevmy.com>
2017-04-15 19:02:24 +0800

    completed lesson 2 and 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    Hello world
+    {{counter}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
   </div>
 </template>
+<script>
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      counter: state => state.counter
+    })
+  },
+
+  methods: {
+    ...mapMutations([
+      'increment',
+      'decrement'
+    ])
+  }
+}
+
+</script>
